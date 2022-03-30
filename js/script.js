7c7a200d62e38c271ce9ed15d8e953d00d1f2ed7
bartoszjas30 <bartoszjas20@gmail.com>
2022-03-30 19:20:20 +0200

    changing let to const

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 
 
-let argButtonName, buttonPaper, buttonRock, buttonScissors, buttonSissors, buttonTest;
-buttonScissors = document.getElementById('button-scissors');
-buttonRock = document.getElementById('button-rock');
+let argButtonName;
+
+const buttonRock = document.getElementById('button-rock');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
-buttonPaper = document.getElementById('button-paper');
+
+const buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
-buttonScissors = document.getElementById('button-scissors');
+
+const buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 
 /**
@@ -15,10 +17,11 @@ buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); }
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-  let argMoveId, argPlayerMove; 
+
 /**
  * Describe this function...
  */
+ let argMoveId;
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
   if (argMoveId == 1) {
@@ -32,11 +35,13 @@ function getMoveName(argMoveId) {
     return 'kamień';
   }
 }
-let argComputerMove;
+
 
 /**
  * Describe this function...
  */
+let argPlayerMove;
+let argComputerMove;
 function displayResult(argPlayerMove, argComputerMove) {
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
@@ -53,14 +58,15 @@ function displayResult(argPlayerMove, argComputerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 
-let playerMove, computerMove, randomNumber, playerInput;
-'papier';
-playerMove = argButtonName;
+
+
+const playerMove = argButtonName;
+let playerInput;
 console.log('wybór ruchu gracza to: ' + playerInput);
 console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
+const computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
 }
